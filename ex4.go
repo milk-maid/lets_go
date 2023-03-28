2be001f4b8e063770fc8fcc0d179a90c628203b3
milk-maid <roqbell@gmail.com>
2023-03-28 12:15:16 +0100

    get firm grip of the basics

---
+++ ex4.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// Create your own type. Have the underlying type be an int.
+// create a VARIABLE of your new TYPE with the IDENTIFIER “x” using the “VAR” keyword
+// in func main
+// print out the value of the variable “x”
+// print out the type of the variable “x”
+// assign 42 to the VARIABLE “x” using the “=” OPERATOR
+// print out the value of the variable “x”
+
+type bello int
+
+var x bello
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42;
+	fmt.Println(x)
+}
